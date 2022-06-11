2545c165a73e11928a020cec53335c75114f2fea
Nadamostafa2 <48297055+Nadamostafa2@users.noreply.github.com>
2022-06-11 19:55:08 +0200

    messenger Design

---
+++ lib/chatData.dart
@@ -0,0 +1,9 @@
+class ChatData {
+  final String? name;
+  final String? msg;
+  final String? time;
+  final String? image;
+
+  final bool? isActive;
+  ChatData(this.name, this.msg, this.time,this.image,{ this.isActive});
+}
